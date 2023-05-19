4256fcf23958acb6c7e52bfadd30ae665fdd9b5e
sumin-yu <ysmsoomin@snu.ac.kr>
2023-05-19 22:31:29 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#inclue<vector>
 
+using namespace std;
+
+int N,M;
+vector<int> vec;
+
+void func(int cnt) {
+
+
+
+
+
+
+
+    }
+
+int main() {
+   cin >> N >> M;
+   func(0);
+}
